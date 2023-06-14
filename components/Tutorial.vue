2edc59a3e7a70eab895e4ac55e1244c9ce20e6dc
Rafael Dias <rafael.ddsociety@gmail.com>
2023-06-14 16:01:07 -0300

    site

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <section>
+    <h1>Deploy 5!</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
